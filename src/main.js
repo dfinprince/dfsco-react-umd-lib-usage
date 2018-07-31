d5b993affaa5bbb6a415915015c2fbe7de83c5c2
dfscoprince <prince.cheruvathur@dfsco.com>
2018-07-31 10:40:30 -0700

    Working application using UNPKG react library

--- src/main.js
+++ src/main.js
@@ -1,18 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { TextInput } from 'dfsco-react-umd-lib'
-
 class MyApp extends React.Component {
   render() {
     return (
-      <div>
+      <div onClick={()=>{umdLibrary.b();umdLibrary.t()}}>
         <h1>Hello from React {React.version}</h1>
         <h2>Test</h2>
-        <TextInput type="text"
-          label="Email Address"
-          value="name@example.com"
-          onChange={x => console.log(x)}
-          onClick={x => console.log(x)} />
+        <umdLibrary.TextInput type="text" label="Email Address" value="name@example.com" onChange={x=>console.log(x)} onClick={x=>console.log(x)} />
       </div>
     );
   }
@@ -22,9 +14,3 @@ ReactDOM.render(
   <MyApp />,
   document.getElementById('root')
 )
-
-
-// ReactDOM.render(
-//   <MyApp />,
-//   document.body
-// )
